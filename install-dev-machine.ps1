deeb39ea6d78982b6616e76c60440ef52d2a5a68
grandmasterjjs <github@grandmasterj.com>
2025-05-24 18:28:46 -0400

    fix: update WSL installation to version 2204 and streamline profile installation

--- install-dev-machine.ps1
+++ install-dev-machine.ps1
@@ -13,7 +13,7 @@ choco install git vscode firefox powertoys windows-terminal `
 
 # Install WSL1 and Ubuntu
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
-choco install wsl-ubuntu-2004 -y
+choco install wsl-ubuntu-2204 -y
 
 # Set Windows Appearance to Dark Mode
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0
@@ -28,8 +28,6 @@ $ompPath = "$env:USERPROFILE\AppData\Local\Programs\oh-my-posh\themes"
 New-Item -Path $ompPath -ItemType Directory -Force | Out-Null
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/kali.omp.json" -OutFile "$ompPath\kali.omp.json"
 
-# Install placeholder profile
-$profileSource = "$PSScriptRoot\profile\profile.ps1"
-$profileTarget = "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
-New-Item -ItemType Directory -Path (Split-Path $profileTarget) -Force | Out-Null
-Copy-Item $profileSource -Destination $profileTarget -Force
+# Install profile
+
+cp $PSScriptRoot\profile\profile.ps1 $PROFILE
